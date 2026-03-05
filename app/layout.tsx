97550004862369d702a924a8c0fb34646d6d87c8
gwhamer1 <gwhamer1@gmail.com>
2026-03-05 11:11:50 -0600

    Fix pixel double fire

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,16 +26,19 @@ export default function RootLayout({
       <head>
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2005844660334436');
-            fbq('track', 'PageView');
+            if (!window._fbqInitialized) {
+              window._fbqInitialized = true;
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2005844660334436');
+              fbq('track', 'PageView');
+            }
           `}
         </Script>
         <noscript>
